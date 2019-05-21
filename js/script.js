945cab1da2f79ceead3a26138dd08a76d3df7a2a
Łukasz Nolka <nolkalukasz@gmail.com>
2019-05-21 14:20:10 +0200

    Add first part code

---
+++ js/script.js
@@ -0,0 +1,103 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    function randomString() {
+        var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ';
+        var str = '';
+        for (var i = 0; i < 10; i++) {
+            str += chars[Math.floor(Math.random() * chars.length)];
+        }
+        return str;
+    } 
+
+    class KanbanManager {
+
+        constructor(selector) {
+            this.selector = selector
+            this.render()
+            this.addListeners()
+        }
+
+        addListeners() {
+            document.querySelector('.create-column').addEventListener('click', (e) => {
+                e.preventDefault();
+                let name = document.querySelector('#column-name');
+            
+                if(name.value == '') {
+                    name.classList.add('error');
+                } else {
+                    name.classList.remove('error');
+                    let column = new Column(name.value, randomString());
+                    name.value = '';                    
+                }
+            });
+
+            const columnContainer = document.querySelector('.column-container');
+            
+            columnContainer.addEventListener('click', (e) => {
+                if(e.target.classList.contains('btn-delete')){
+                    e.target.parentElement.remove();
+                }
+            });
+
+            document.querySelector('.column-container').addEventListener('click', (e) => {
+                if(e.target.classList.contains('add-card')){                   
+                    let table = e.target.parentElement.querySelector('.column-card-list');
+                    const card = new Card('Nowe Zadanie', randomString(), table);
+                }
+            });
+
+        }
+
+        render() {
+            this.selector.innerHTML = `            
+            <div id="board" class="board">
+                <h1>Kanban board</h1>
+                    <form>
+                        <input id="column-name" class="column-name" type="text" placeholder="Nazwa Kolumny">
+                        <button class="create-column">Add a column</button>
+                    </form>
+                <div class="column-container"></div>
+            </div>`;
+        }       
+    }
+
+    class Column {
+        constructor(name, id) {
+            this.name = name 
+            this.id = id
+            this.addColumn()
+            
+        }
+
+        addColumn() {
+            document.querySelector('.column-container').innerHTML += `
+                <div class="column">
+                    <h2 class="column-title">${this.name}</h2>
+                    <ul id=${this.id} class="column-card-list"></ul>
+                    <button class="btn-delete">X</button>
+                    <button class="add-card">Add a card</button>
+                </div>
+            `;       
+        }
+    }
+
+    class Card {
+        constructor(name,id, table) {
+            this.name = name
+            this.id = id 
+            this.table = table
+            this.addCard()
+        }
+
+        addCard() {
+            this.table.innerHTML += `
+            <li id="${this.id}" class="card">${this.name}</li>
+            `;
+        }
+
+    }
+
+
+    const app = new KanbanManager(document.querySelector('#app'));
+    const asd = new Column('To Do', randomString());
+});
